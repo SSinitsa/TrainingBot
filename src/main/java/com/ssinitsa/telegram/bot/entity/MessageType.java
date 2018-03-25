7a75152d9448a96366310ca27f0a45b6d0dffdcf
Sergey <sergeysinitsa@gmail.com>
2018-03-26 02:08:30 +0300

    add models, add repository

---
+++ src/main/java/com/ssinitsa/telegram/bot/entity/MessageType.java
@@ -0,0 +1,4 @@
+package com.ssinitsa.telegram.bot.entity;
+
+public class MessageType {
+}
---
+++ src/main/java/com/ssinitsa/telegram/bot/repository/TGMessageRepository.java
@@ -0,0 +1,4 @@
+package com.ssinitsa.telegram.bot.repository;
+
+public interface TGMessageRepository {
+}
